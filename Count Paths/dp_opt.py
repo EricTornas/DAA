2e18266e782a14d43ec3a9299698a33f096f01c3
Eric <eric.tornas@gmail.com>
2024-09-25 20:03:09 -0400

    update Count Paths

--- Count Paths/dp_opt.py
+++ Count Paths/dp_opt.py
@@ -1,42 +1,34 @@
 from collections import defaultdict
 
-def dfs(u, par=-1):
-    global ans
-    fq[u][col[u]] = 1
+def dfs(u, parent, colors, adj, fq):
+    global count
+    fq[u][colors[u]] = 1
 
     for v in adj[u]:
-        if v == par:
+        if v == parent:
             continue
-
-        dfs(v, u)
-
+        dfs(v, u, colors, adj, fq)
         if len(fq[v]) > len(fq[u]):
             fq[v], fq[u] = fq[u], fq[v]  # swap de diccionarios
-
         for p in fq[v].items():
-            ans += fq[u].get(p[0], 0) * p[1]
+            count += fq[u].get(p[0], 0) * p[1]
             fq[u][p[0]] = fq[u].get(p[0], 0) + p[1]
 
-        fq[u][col[u]] = 1
-
-
+        fq[u][colors[u]] = 1
 
 t = int(input())
 for _ in range(t):
-    ans = 0
+    count = 0
+
     n = int(input())
     adj =  [[] for _ in range(n)]
     fq = [defaultdict(int) for _ in range(n)]
-    col  = list(map(int,input().split()))
-        
+    colors  = list(map(int,input().split()))
 
     for _ in range(n - 1):
         u, v = map(int, input().split())
-        u -= 1
-        v -= 1
-        adj[u].append(v)
-        adj[v].append(u)
+        adj[u-1].append(v-1)
+        adj[v-1].append(u-1)
 
-    
-    dfs(0)
-    print(ans)
+    dfs(0, -1, colors, adj, fq)
+    print(count)
